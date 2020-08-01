c64edb866ef400da058b74571231944435d916c2
hiamalaya <hiamalaya777@gmail.com>
2020-08-01 11:23:09 +0530

    some stuff done

--- src/Components/cardList.js
+++ src/Components/cardList.js
@@ -6,9 +6,9 @@ const CardList = ({info}) => {
 	//console.log("Checking for array");
 	//console.log(info.isArray());
 	//console.log(info);
-	if(true){
+	/*if(true){
 		throw new Error("ERROR!!!!");
-	}
+	}*/
 
 	const cardArray = info.map((user, index) => {
 			return(<Card key={info[index].id} id={info[index].id} name={info[index].name} email={info[index].email} />);
